3ef953e4f3a72bf75395417f2a4708b4fb420d16
adtemp <61062427+adtemp@users.noreply.github.com>
2020-02-15 03:16:41 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.setAttribute('rel','stylesheet');
+link.setAttribute('href','https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css');
+head.appendChild(link);
